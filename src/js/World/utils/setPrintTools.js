aaa42c4cc296a3c0bdf8429cd55e34637f9a1e67
birdlaketree <birdlaketree@gmail.com>
2023-06-14 10:32:40 +0200

    add redraw with rnd params method in print tools

--- src/js/World/utils/setPrintTools.js
+++ src/js/World/utils/setPrintTools.js
@@ -3,6 +3,21 @@ export const setPrintTools = (renderer, composer, postprocessingEnabled, scene, 
     if (e.code === 'KeyP') {
       saveAsPng();
     }
+    if (e.code === 'KeyE') {
+      console.log('E');
+      m0 = Math.random();
+      m1 = Math.random();
+      m2 = Math.random();
+      m3 = Math.random();
+      m4 = Math.random();
+      drawArt();
+    }
+    if (e.code === 'KeyC') {
+      console.log('C');
+      // m1 = Math.random();
+      randomM1();
+      drawArt();
+    }
   }
 
   const saveAsPng = () => {
