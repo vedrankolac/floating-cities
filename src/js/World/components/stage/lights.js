8fef432da5c5c6701a65e52ec934e217a74ed4d5
birdlaketree <birdlaketree@gmail.com>
2023-06-16 11:46:22 +0200

    remove spot light

--- src/js/World/components/stage/lights.js
+++ src/js/World/components/stage/lights.js
@@ -2,7 +2,8 @@ import { GUI } from 'dat.gui';
 import { 
   AmbientLight,
   SpotLight,
-  SpotLightHelper
+  SpotLightHelper,
+  Vector3
 } from 'three';
 import { PerlinNoise } from '../canvasMaps/PerlinNoise';
 
@@ -13,39 +14,44 @@ export const createLights = scene => {
   // setting lower mapSize makes it much faster on iPhone 12 Pro Max
   // const spot = new SpotLight(0xffffff, 840);
 
-  const spot = new SpotLight(0xffffff, 24);
-  spot.penumbra = 1;
-  spot.decay = 0.8;
-  spot.angle = Math.PI/1.6;
-  // spot.position.set(
-  //   Math.random() * 2 + 1,
-  //   6,
-  //   Math.random() * 2 + 1,
-  // );
-  spot.castShadow = true;
-  spot.map = map.colorMap;
-  spot.shadow.focus = 1;
-  spot.shadow.mapSize.width = 4096;
-  spot.shadow.mapSize.height = 4096;
-  scene.add(spot);
-  map.colorMap = null;
-  spot.target.position.set(0, 0, 0);
-  spot.target.updateMatrixWorld();
+  // const spot = new SpotLight(0xffffff, 24);
+  // spot.penumbra = 1;
+  // spot.decay = 0.8;
+  // spot.angle = Math.PI/1.6;
+  // // spot.position.set(
+  // //   Math.random() * 2 + 1,
+  // //   6,
+  // //   Math.random() * 2 + 1,
+  // // );
+  // spot.castShadow = true;
+  // spot.map = map.colorMap;
+  // spot.shadow.focus = 1;
+  // spot.shadow.mapSize.width = 4096;
+  // spot.shadow.mapSize.height = 4096;
+  // scene.add(spot);
+  // map.colorMap = null;
+  // spot.target.position.set(0, 0, 0);
+  // spot.target.updateMatrixWorld();
   // scene.add(new SpotLightHelper(spot));
 
   const ambient = new AmbientLight(0xffffff, 3.0); // soft white light
   scene.add(ambient);
 
-  const showGui = false;
+  // const showGui = false;
 
-  if (showGui) {
-    const gui = new GUI();
-    // gui.close()
-    gui.add(spot, 'intensity', 0.0, 100 );
-    gui.add(spot, 'penumbra', 0.0, 2 );
-    gui.add(spot, 'decay', 0.0, 2 );
-    gui.add(spot, 'angle', 0.0, 2 );
-    gui.add(ambient, 'intensity', 0.0, 20.0 );
+  // if (showGui) {
+  //   const gui = new GUI();
+  //   // gui.close()
+  //   gui.add(spot, 'intensity', 0.0, 100 );
+  //   gui.add(spot, 'penumbra', 0.0, 2 );
+  //   gui.add(spot, 'decay', 0.0, 2 );
+  //   gui.add(spot, 'angle', 0.0, 2 );
+  //   gui.add(ambient, 'intensity', 0.0, 20.0 );
+  // }
+
+  // hack for getting the same random seed
+  const spot = {
+    position: new Vector3()
   }
 
   return {
